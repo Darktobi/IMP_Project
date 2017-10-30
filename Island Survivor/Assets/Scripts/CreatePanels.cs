56594a4ba1fdb6a37e5aa07cd8b2082f988c1cf2
Yannick C <Yannick.collin95@gmail.com>
2017-10-30 19:34:41 +0100

    Layout Concept

---
+++ Island Survivor/Assets/Scripts/CreatePanels.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreatePanels : MonoBehaviour {
+
+    public int numberPanels;
+    public GameObject button;
+
+    // Use this for initialization
+    void Start () {
+		for (int i = 0; i<numberPanels; i++)
+        {
+            GameObject newButton = Instantiate(button) as GameObject;
+            newButton.transform.SetParent(gameObject.transform, false);
+        }
+	}
+
+    
+
+
+}
---
+++ Island Survivor/Assets/Scripts/SurvivorInformation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurvivorInformation : MonoBehaviour {
+
+    public string gender;
+    public int age;
+    public string job;
+}
